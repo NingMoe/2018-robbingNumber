d66490e789186e25fe964af61067a53d915ecbd2
Tosn <taoserking@163.com>
2018-11-20 15:57:25 +0800

    删除首页结束弹出

--- src/views/index.vue
+++ src/views/index.vue
@@ -155,10 +155,10 @@
             this.countDown();
             if (new Date().getTime() > new Date(this.timeSelect[this.timeSelect.length-1].endTime).getTime()) {
                 this.activeEnd = true;
-                this.$messagebox({
-                    message: '<div class="index-popend">活動已結束，配對成功的用戶，請在7天內前往附近門店辦理，逾期號碼將失效。</div>',
-                    confirmButtonText: '我知道了'
-                })
+                // this.$messagebox({
+                //     message: '<div class="index-popend">活動已結束，配對成功的用戶，請在7天內前往附近門店辦理，逾期號碼將失效。</div>',
+                //     confirmButtonText: '我知道了'
+                // })
             }
         },
         components: {
